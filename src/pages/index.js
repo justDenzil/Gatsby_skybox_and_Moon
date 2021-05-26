fe2c8516c7d13898535ac94860fb9bf0a6b0bb0b
justDenzil <vikash.denzil@gmail.com>
2021-05-26 20:20:24 +0530

    Added moon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,58 @@
-import React from "react"
+import React, { useRef, Suspense } from "react"
+import { Canvas, extend, useThree, useFrame, useLoader } from "@react-three/fiber"
+import { 
+  TextureLoader,
+  CubeTextureLoader } from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "../styles/styles.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  const { camera, gl: { domElement }} = useThree();
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      autoRotate={true}
+      enableZoom={false}
+    />
+  );
+};
+
+const Skybox = () => {
+  const { scene } = useThree();
+  const loader = new CubeTextureLoader();
+  const texture = loader.load([
+    "/haloweave.png",
+    "/2.png",
+    "/3.png",
+    "/4.png",
+    "/5.png",
+    "/6.png"
+  ]);
+  scene.background = texture;
+  return null;
 }
+
+const Sphere = () => {
+  const texture = useLoader(TextureLoader, "/texture.jpg");
+  return (
+    <mesh>
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial attach="material" map={texture} />
+    </mesh>
+  )
+}
+
+export default () => (
+  <Canvas>
+    <CameraControls />
+    <Suspense fallback={null}>
+      <Skybox />
+      <Sphere />
+    </Suspense>
+  </Canvas>
+)
